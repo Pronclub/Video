ffb6d6352145a2285eb69095e9d80ba477569571
Pronclub <stayslanka@gmail.com>
2025-11-06 23:41:25 +0530

    Update ads-system.js

--- ads/ads-system.js
+++ ads/ads-system.js
@@ -2,21 +2,15 @@
 
 // Ad links
 const adLinks = [
-                    "https://otieu.com/4/9835089",
-  "https://eminencehillsidenutrition.com/h8junpek?key=26fe0739e1e80a8ffa5ecfab44189a38",
-  "https://eminencehillsidenutrition.com/dw7tr9ztu1?key=0a50f02fa82b9007f3f30efcc3464991",
-  "https://eminencehillsidenutrition.com/wydvnkk8?key=f309dfb4ebd925293f0da3da6dbccfe9",
-  "https://eminencehillsidenutrition.com/dv4carz8?key=ace1ef81105d316bb3ea53f6c9904802",
-  "https://www.effectivegatecpm.com/v37cfafhr?key=cc12415044cd7578b9a4c3abf7e8a7f7", //... Hiran Ads Link ...//
-  "https://otieu.com/4/9835092", //... Hansaka Ads Link ...//
-  "https://www.effectivegatecpm.com/ndp5e9aww3?key=4efa3e2fabbac771694e2c3791bc9014", //... Share Account Ads Link ...//
-  "https://www.effectivegatecpm.com/uccum8g1j?key=dbdc91a1494138cc50a17be31032ce44", //... User-1 Ads Link ...//
-  "https://otieu.com/4/9835091", //... User-2 Ads Link ...//
-  "https://otieu.com/4/9835088" //... User-3 Ads Link ...//
+                    "https://eminencehillsidenutrition.com/s5v92wjc11?key=b1d1b7fc017831ec983c6d1aae5b5628",
+                    "https://eminencehillsidenutrition.com/h8junpek?key=26fe0739e1e80a8ffa5ecfab44189a38",
+                    "https://eminencehillsidenutrition.com/dw7tr9ztu1?key=0a50f02fa82b9007f3f30efcc3464991",
+                    "https://eminencehillsidenutrition.com/wydvnkk8?key=f309dfb4ebd925293f0da3da6dbccfe9",
+                    "https://eminencehillsidenutrition.com/dv4carz8?key=ace1ef81105d316bb3ea53f6c9904802"
 ];
 
 // Direct link for first click
-const directLink = "https://eminencehillsidenutrition.com/s5v92wjc11?key=b1d1b7fc017831ec983c6d1aae5b5628"; //... Share Account Ads Link ...//
+const directLink = "https://otieu.com/4/9835089"; //... Share Account Ads Link ...//
 
 // Popunder links (used by triggerPopunder)
 const popunderLinks = [
